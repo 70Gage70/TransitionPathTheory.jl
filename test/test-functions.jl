89ac760629d8fd766b9300611b33039dd222eaa6
Gage <gage.bonner@gmail.com>
2023-05-09 18:09:39 -0500

    tests?

--- test/test-functions.jl
+++ test/test-functions.jl
@@ -17,7 +17,9 @@ function tpt_test(ftest::String, tpt_result::AbstractTPTHomogResult)
                 tpt = getfield(tpt_result.sets, fns)
                 test = read(testf["tpt_homog/indices/$fns"])
         
-                if !test_eq(tpt, test) 
+                if !test_eq(tpt, test)
+                    @show tpt
+                    @show test
                     @error("$fns mismatch") 
                     return false
                 end 
@@ -27,6 +29,8 @@ function tpt_test(ftest::String, tpt_result::AbstractTPTHomogResult)
             test = read(testf["tpt_homog/statistics/$fn"])
             
             if !test_eq(tpt, test) 
+                @show tpt
+                @show test
                 @error("$fn mismatch") 
                 return false
             end 
