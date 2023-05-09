9a9eb92e65d64102b048a6d44d5644bdbf0d87fd
Gage <gage.bonner@gmail.com>
2023-05-09 18:31:35 -0500

    test tol 1e-15

--- test/test-functions.jl
+++ test/test-functions.jl
@@ -4,7 +4,7 @@ function test_eq(a::Any, b::Any)
     elseif length(a) != length(b)
         return false
     else
-        return all(isapprox.(a, b, atol = 1e-16))
+        return all(isapprox.(a, b, atol = 1e-15))
     end
 end
 
