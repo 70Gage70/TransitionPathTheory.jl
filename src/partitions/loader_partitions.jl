9a520571365077a12a3d8b1400c78eaf4f8e822f
Gage <gage.bonner@gmail.com>
2023-05-09 19:41:00 -0500

    new P test

--- src/partitions/loader_partitions.jl
+++
@@ -1,6 +0,0 @@
-include("../types_loader.jl")
-include("../workspaces.jl")
-include("partitions.jl")
-
-mp_stat = minimal_partitions(tpt_homog, tpt_stationary)
-mp_nonstat = minimal_partitions(tpt_homog, tpt_nonstationary)
